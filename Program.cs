b01137555875fb248b129012a6074a515780f067
PenaPC <luis_alejandro241096@hotmail.com>
2019-11-15 11:48:29 -0600

    CSV done

--- Program.cs
+++ Program.cs
@@ -3,9 +3,13 @@ using System.Linq;
 using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Text;
+// using CsvHelper;
 
 using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
 
 namespace MDPExcersise
 {
@@ -43,6 +47,9 @@ namespace MDPExcersise
     class MDPGrid{
         public Node[,] grid;
         public double alpha = 0.9;
+
+        public List<List<object>> history = new List<List<object>>();
+        public List<List<object>> directionHistory = new List<List<object>>();
 
         public IDictionary<int, string> dict = new Dictionary<int, string>();
         
@@ -119,7 +126,23 @@ namespace MDPExcersise
             this.grid[i,j].direction = direction;
             this.grid[i,j].value = cellValue;
         }
-
+        public void saveState(int ic, int jc){
+            List<object> newentry = new List<object>();
+            List<object> newentryDirection = new List<object>();
+            for(int i = 0; i<this.grid.GetLength(0);i++){
+                for(int j = 0; j<this.grid.GetLength(1);j++){
+                    newentry.Add(this.grid[i,j].value);
+                    newentryDirection.Add(this.grid[i,j].direction);
+                }
+                this.history.Add(newentry);
+                this.directionHistory.Add(newentryDirection);
+                newentry = new List<object>();
+                newentryDirection = new List<object>();
+            }
+            newentry = new List<object>(){"value",this.grid[ic,jc],"-","-","-","-","-","-"};
+            this.history.Add(newentry);
+            this.directionHistory.Add(newentry);
+        }
         public void evalGrid(){
             for(int i = 0; i<this.grid.GetLength(0);i++){
                 for(int j = 0; j<this.grid.GetLength(1);j++){
@@ -127,6 +150,7 @@ namespace MDPExcersise
                         this.grid[i,j].value = this.grid[i,j].value;
                     }else{
                         newValue(i,j);
+                        saveState(i,j);
                     }
                 }
             }
@@ -196,24 +220,102 @@ namespace MDPExcersise
             static readonly string sheet = "sheetsAPI";
             static readonly string SpreadsheetId = "1zooAOBAVr90wHI-CGAtAa_UG6fwdvXsacuPBPFJc5gE";
             static SheetsService service;
+        static void Init(){
+            GoogleCredential credential;
+            //Reading Credentials File...
+            using (var stream = new FileStream("app_client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleCredential.FromStream(stream)
+                    .CreateScoped(Scopes);
+            }
+            // Creating Google Sheets API service...
+            service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+        static void AddRow(MDPGrid grid) { 
+            // Specifying Column Range for reading...
+            var range = $"{sheet}!A:N";
+            var valueRange = new ValueRange();
+            // Data for another Student...
+            var oblist = new List<object>() { "Harry", "80", "77", "62", "98" };
+            foreach (var row in grid.history)
+            {
+                oblist = row;
+                valueRange.Values = new List<IList<object>> { oblist };
+                // Append the above record...
+                var appendRequest = service.Spreadsheets.Values.Append(valueRange, SpreadsheetId, range);
+                appendRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+                var appendReponse = appendRequest.Execute();
+            }
+
+            //direction printing
+            // Specifying Column Range for reading...
+            // range = $"{sheet}!P:Z";
+            // valueRange = new ValueRange();
+            // // Data for another Student...
+            // oblist = new List<object>() { "Harry", "80", "77", "62", "98" };
+            // foreach (var row in grid.directionHistory)
+            // {
+            //     oblist = row;
+            //     valueRange.Values = new List<IList<object>> { oblist };
+            //     // Append the above record...
+            //     var appendRequest = service.Spreadsheets.Values.Append(valueRange, SpreadsheetId, range);
+            //     appendRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+            //     var appendReponse = appendRequest.Execute();
+            // }
+            
+        }
+        static String makeCSV(MDPGrid grid) {
+            string csv = "";
+            foreach (var row in grid.history)
+            {
+                foreach (var cell in row)
+                {
+                    csv += cell+",";
+                }
+                csv.Remove(csv.Length - 1);
+                csv += "\n";
+            }
+            return csv;
+        }
+        static String makeCSVDirs(MDPGrid grid) {
+            string csv = "";
+            foreach (var row in grid.directionHistory)
+            {
+                foreach (var cell in row)
+                {
+                    csv += cell+",";
+                }
+                csv.Remove(csv.Length - 1);
+                csv += "\n";
+            }
+            return csv;
+        }
 
         static void Main(string[] args)
         {
 
             Console.WriteLine("stuff");
-            MDPGrid grid = new MDPGrid(5,5);
-            Position[] goalPositions = new Position[2]{new Position(0,3),new Position(4,4)};
-            Position[] wallPositions = new Position[4]{new Position(1,0),new Position(1,4),new Position(2,2),new Position(3,3)};
+            MDPGrid grid = new MDPGrid(10,15);
+            Position[] goalPositions = new Position[2]{new Position(9,9),new Position(0,14)};
+            Position[] wallPositions = new Position[13]{new Position(1,2),new Position(3,2),new Position(5,2),new Position(7,2),new Position(9,2),new Position(0,5),new Position(2,5),new Position(4,5),new Position(6,5),new Position(8,5),new Position(1,9),new Position(3,9),new Position(5,9)};
             int[] goalValues = new int[2]{10,20};
 
             grid.setWalls(wallPositions);
             grid.setGoals(goalPositions, goalValues);
             Console.WriteLine(grid.drawGrid());
+            Console.WriteLine(grid.drawGridDirection());
             grid.evalGrid();
             Console.WriteLine(grid.drawGrid());
             Console.WriteLine(grid.drawGridDirection());
         
-
+            // Init();
+            // AddRow(grid);
+            File.WriteAllText("valuedata.csv", makeCSV(grid));
+            File.WriteAllText("directiondata.csv", makeCSVDirs(grid));
             
         }
     }
